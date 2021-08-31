e42737cbd5ef24458a6e636e2e659ed587b6c805
acsaeng <acsaeng@gmail.com>
2021-08-31 15:10:20 -0600

    Edited code formatting

--- Control Systems/Programming/ControllerCode/liftPlatform.ino
+++ Control Systems/Programming/ControllerCode/liftPlatform.ino
@@ -1,7 +1,6 @@
 // Function that adjusts the angle of the base
 
 void liftPlatform() {
-
     // Servo sweep that lifts the platform
       for (int platformAngle = 0; platformAngle <= 60; platformAngle += 1) {
       liftingMotor.write(platformAngle); 
--- Control Systems/Programming/ControllerCode/shootBall.ino
+++ Control Systems/Programming/ControllerCode/shootBall.ino
@@ -2,8 +2,8 @@
 
 void shootBall() { 
   //  Kinematic equation that calculates the angular speed necessary to launch the ball
-  angularSpeed = 0.792804694892816*pow(distance,5) - 6.49040934100877*pow(distance,4) + 20.5275746574592*pow(distance,3) - 32.8989742874332*pow(distance,2) + 37.3396046930129*distance + 0.389709401814137;
-  rpm = angularSpeed*(30/3.141592653); // Converts angular speed in rad/s to rpm
+  angularSpeed = 0.792804694892816 * pow(distance, 5) - 6.49040934100877 * pow(distance, 4) + 20.5275746574592 * pow(distance, 3) - 32.8989742874332 * pow(distance, 2) + 37.3396046930129 * distance + 0.389709401814137;
+  rpm = angularSpeed * (30 / 3.141592653); // Converts angular speed in rad/s to rpm
   rpm = (int) rpm; // Converts float to integer
 
   shootingStepper.begin(rpm, microsteps); // Sets the speed of the motor
